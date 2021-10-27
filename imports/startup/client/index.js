f311fd94faaf7c03d7b341b801674de0dea8763d
Gerardo Miranda <gerardodev25@gmail.com>
2021-10-27 19:14:12 -0400

    integrando vuetify

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,10 +1,13 @@
 import { Meteor } from "meteor/meteor";
 import Vue from "vue";
+import vuetify from "../../ui/plugins/vuetify";
 
+import "../../ui/plugins";
 import App from "/imports/ui/App";
 
 Meteor.startup(() => {
   new Vue({
+    vuetify,
     render: (e) => e(App),
   }).$mount((elementOrSelector = "app"));
 });
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>hola mundo desde vue</div>
+  <v-app>
+    <v-container>
+      <h1>hola mundo desde vue</h1>
+      <!-- <v-btn color="primary">Hola</v-btn>
+      <v-btn color="secondary">adios</v-btn> -->
+
+      <!-- <v-bottom-navigation v-model="value" :background-color="color" dark shift>
+        <v-btn>
+          <span>Video</span>
+
+          <v-icon>mdi-television-play</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Music</span>
+
+          <v-icon>mdi-music-note</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Book</span>
+
+          <v-icon>mdi-book</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>Image</span>
+
+          <v-icon>mdi-image</v-icon>
+        </v-btn>
+      </v-bottom-navigation> -->
+    </v-container>
+  </v-app>
 </template>
 
 <script>
